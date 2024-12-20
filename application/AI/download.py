e234d31587d9901f2bcd2fcc2af01f160b10622d
Wiztek712 <raban.quentin@gmail.com>
2024-12-20 16:26:46 +0100

    add a model and improve download function

--- application/AI/download.py
+++ application/AI/download.py
@@ -1,6 +1,8 @@
 import urllib.request
+import urllib.parse
 import pandas as pd
 import ujson as json
+import os
 
 def download_file(url, destination_path):
     try:
@@ -15,11 +17,33 @@ def parse_ndjson_pandas(file_path):
         records = map(json.loads, open(file_path))
         df = pd.DataFrame.from_records(records)
         print(df.tail(10))
+        return df
     except Exception as e:
          print(f"Failed to parse file. Error: {e}")
 
+def download_data_and_parse_it(destination_path):
+    if os.path.exists(destination_path):
+        print("The file exists.")
+        return parse_ndjson_pandas(destination_path)
+    else:
+        print("The file does not exist.")
+        # Get the file name without extension
+        file_name = os.path.splitext(os.path.basename(destination_path))[0]
+        
+        # URL encode the file name to handle special characters
+        encoded_file_name = urllib.parse.quote(file_name)
+        
+        # Construct the URL using the encoded file name
+        url = f"https://storage.googleapis.com/quickdraw_dataset/full/simplified/{encoded_file_name}.ndjson"
+
+        # Now you can call the download and parse function
+        file_path = download_file(url, destination_path)
+        return parse_ndjson_pandas(file_path)
+
 if __name__ == "__main__":
-    url = "https://storage.googleapis.com/quickdraw_dataset/full/simplified/The%20Eiffel%20Tower.ndjson"
-    destination_path = "The_Eiffel_Tower.ndjson"
-    file_path = download_file(url, destination_path)
-    parse_ndjson_pandas(file_path)
+    file_path = "star.ndjson"
+    df = download_data_and_parse_it(file_path)
+    file_path = "sword.ndjson"
+    df1 = download_data_and_parse_it(file_path)
+    file_path = "tent.ndjson"
+    df2 = download_data_and_parse_it(file_path)
