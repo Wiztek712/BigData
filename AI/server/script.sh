f06fb3c3877ca5cb81dc7af8766cb7ede0cbf900
Wiztek712 <raban.quentin@gmail.com>
2025-02-13 12:39:57 +0100

    add flask server

---
+++ AI/server/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the URL of the Flask server and the path to the model file
+MODEL_URL="http://localhost:5001/upload_model"
+WORD_URL="http://localhost:5001/upload_words"
+MODEL_FILE="deepest_model64_23labels.pth"  # Path to your .pth model file
+WORD_FILE="words.json"
+
+# Use curl to send the POST request with the model file as form-data
+curl -X POST -F "model=@$MODEL_FILE" $MODEL_URL
+
+curl -X POST -H "Content-Type: application/json" -d @$WORD_FILE $WORD_URL
